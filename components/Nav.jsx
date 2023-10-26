e7eac8994b94bf3f59bf143ce520d971efca4e89
Stefan Page <90534062+stefpage@users.noreply.github.com>
2023-10-26 17:24:10 -0400

    minor css fixes

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -23,7 +23,7 @@ const Nav = () => {
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
         <Image
-          src="/assets/images/logo.svg "
+          src="/assets/images/logo.svg"
           alt="Promtopia logo"
           width={30}
           height={30}
